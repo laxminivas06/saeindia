e6bdb70130e76089007305894c6787ac251932be
laxminivas06 <laxminivasmorishetty143@gmail.com>
2026-09-24 17:08:26 +0530

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     host: true,
-    port: 5173,
-    https: true
+    port: 5173
   }
 });
+
